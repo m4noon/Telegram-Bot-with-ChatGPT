a2e5dae8d0903e50d3942b5be034bb3f37190433
m4noon <thejonka@gmail.com>
2023-02-14 22:54:35 +0300

    send info about not auth user

--- main.py
+++ main.py
@@ -27,6 +27,7 @@ def send_welcome(message):
         print(f'New user? {message.from_user.username} and ID: {message.from_user.id}')
         bot.send_message(chat_id=message.from_user.id,
                          text=f'Вы не имеете доступа {message.from_user.username}')
+        bot.send_message(chat_id=authorized_users[0], text=f'Новый пользователь:\n{message.from_user}')
 
 
 @bot.message_handler(func=lambda _: True)
@@ -39,8 +40,8 @@ def chatgpt(message):
             prompt=message.text,
             temperature=0.5,
             max_tokens=1000,
-            top_p=1,
-            frequency_penalty=0.5,
+            top_p=1.0,
+            frequency_penalty=0.0,
             presence_penalty=0.0,
         )
         chat = response['choices'][0]['text']
